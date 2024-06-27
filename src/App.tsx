cd98aaf81b2dd8c03a6421c0d571b7d3b5bc7679
thiger7 <adf0423@gmail.com>
2024-06-27 22:00:12 +0900

    React Hook Form を installして適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Todo from "./components/Todo";
 import Description from "./components/Description";
+import LoginForm from "./components/LoginForm";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <Header />
         <Description />
         <Todo />
+        <LoginForm />
       </div>
     </>
   );
---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,58 @@
+import "@/App.css";
+import { useForm } from "react-hook-form";
+
+interface LoginForm {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({ mode: "onChange"});
+
+  const onSubmit = (data: LoginForm) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="form-cotainer">
+      <h1>Login Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="名前">名前</label>
+        <input
+          id="name"
+          type="text"
+          {...register("name", {
+            required: "名前は必須です。",
+            minLength: { value: 4, message: "4文字以上で入力してください。" },
+          })}
+        />
+        <p>{errors.name?.message as React.ReactNode}</p>
+        <label htmlFor="メールアドレス">メールアドレス</label>
+        <input
+          id="email"
+          type="email"
+          {...register("email", { required: "Emailは必須です。" })}
+        />
+        <p>{errors.email?.message as React.ReactNode}</p>
+        <label htmlFor="パスワード">パスワード</label>
+        <input
+          id="password"
+          type="password"
+          {...register("password", {
+            required: "パスワードは必須です。",
+            minLength: { value: 6, message: "6文字以上で入力してください。" },
+          })}
+        />
+        <p>{errors.password?.message as React.ReactNode}</p>
+        <button type="submit">送信</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
